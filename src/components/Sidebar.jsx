74249a25ea2f7a07b4f88ef3e65d27fd93fe5c66
Abigail Yanos <ab841661@ucf.edu>
2025-02-04 13:15:04 -0500

    SideBar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,27 +4,37 @@ import { useState, useCallback } from "react"
  * unordered list.
  * @returns Component
  */
-export default function Sidebar() {
+export default function Sidebar(initialMenuItems) {
   let [newMenuItem, setNewMenuItem] = useState("")
   // TODO: 2 Using a state hook, maintain the current menu items as an array state.
-  // let [menuItems, setMenuItems] = useState(initialMenuItems)
+  let [menuItems, setMenuItems] = useState(initialMenuItems)
   let [filter, setFilter] = useState("")
   // Adds a single string passed in as parameter to the state element
   // "menuItems" that holds the set of current menu items.
   let addMenuItem = useCallback(() => {
     console.log("Added menu item")
-    //   // TODO: 3. Add a new menu item to the correct variable associated with this class.
-    //   // This involves adding a parameter and changing a class instance variable (props).
-    //   setMenuItems([item, ...menuItems])
-  }, [])
+    if (newMenuItem.trim()) {
+      setMenuItems((prevItems) => [newMenuItem, ...prevItems]);
+    }
+    
+  }, [newMenuItem]);
 
   // TODO: 4. Display ONLY the menu items that contain the filter element value
   // "term" in them. Each menu item should be an unordered list item wrapped in an unordered list (ul) element.
-
+let filterRegex = filter ? new RegExp(filter, "i") : null;
   // TODO: 1 Render inside the outer div an unordered list of the menu items, with each string in the array
   // its own item.
   return (
     <div>
+
+      <ul>
+        {menuItems
+        .filter((item) => (filterRegex ? filterRegex.test(item) : true))
+        .map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+
       <input
         type="text"
         id="newMenuItemValue"
